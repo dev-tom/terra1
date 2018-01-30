9db285118786f41c57d7102da79acc9b2e799fd3
Tom <tflynn@playmigo.tv>
2018-01-30 17:21:08 +0000

    docker demo

--- docker-demo-3/s3.tf
+++ docker-demo-3/s3.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "terraform-state" {
-    bucket = "terraform-state-f28c6"
+    bucket = "terraform-state-a2b6218"
     acl = "private"
-    force_destroy = true
 
     tags {
         Name = "Terraform state"
